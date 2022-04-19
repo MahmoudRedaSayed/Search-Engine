0e809763c07cb670326adf94eba6ca5f548b753d
MUSTAFA-Hamzawy <hamzawy2019579@gmail.com>
2022-04-19 23:49:44 +0200

    nothing

--- src/ServletsPackages/ServletPackage/QueryDivide.java
+++ src/ServletsPackages/ServletPackage/QueryDivide.java
@@ -1,31 +1,31 @@
-
-package ServletsPackages.ServletPackage;
-
-import org.json.JSONArray;
-
-import java.io.IOException;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
-import java.util.ArrayList;
-
-public class QueryDivide extends HttpServlet {
-    public String results;
-    public void setDivided(String Query)
-    {
-        results=Query;
-    }
-    public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
-    {
-        res.addHeader("Access-Control-Allow-Origin","http://localhost:3000");
-        res.setContentType("application/json");
-        res.getWriter().write("Mahmoud");
-    }
-
-}
-
-
-
-
-
 //
+//package ServletsPackages.ServletPackage;
+//
+//import org.json.JSONArray;
+//
+//import java.io.IOException;
+//import javax.servlet.*;
+//import javax.servlet.http.*;
+//import java.io.*;
+//import java.util.ArrayList;
+//
+//public class QueryDivide extends HttpServlet {
+//    public String results;
+//    public void setDivided(String Query)
+//    {
+//        results=Query;
+//    }
+//    public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+//    {
+//        res.addHeader("Access-Control-Allow-Origin","http://localhost:3000");
+//        res.setContentType("application/json");
+//        res.getWriter().write("Mahmoud");
+//    }
+//
+//}
+//
+//
+//
+//
+//
+////
