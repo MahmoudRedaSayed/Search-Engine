d99c30107cb7b2f7231e589e7b632b267dec0c40
MohamedWw <mohamedwalid1960@gmail.com>
2022-04-20 02:26:09 +0200

    Removed DataBase from Servlets

--- src/ServletsPackages/ServletPackage/QuerySearch.java
+++ src/ServletsPackages/ServletPackage/QuerySearch.java
@@ -72,7 +72,7 @@ public class QuerySearch extends HttpServlet {
             res.getWriter().println("phrase"+count);
 
 
-            PhraseSearching obj = new PhraseSearching();
+            //PhraseSearching obj = new PhraseSearching();
 
 //            try {
 ////                 results  =obj.run(searchingQuery,rankerArray,dividedQuery);
@@ -83,12 +83,12 @@ public class QuerySearch extends HttpServlet {
             //call function of query processing
             res.getWriter().println("query"+count);
 
-//            QueryProcessing obj = new QueryProcessing();
-//            try {
-//                 results  =obj.run(searchingQuery,rankerArray,dividedQuery);
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//            }
+            QueryProcessing obj = new QueryProcessing();
+            try {
+                 results  =obj.run(searchingQuery,rankerArray,dividedQuery);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             res.getWriter().println(results);
 
         }
@@ -100,131 +100,131 @@ public class QuerySearch extends HttpServlet {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    public class PhraseSearching {
-        DataBase dataBaseObject = new DataBase();
-        private Map<String, File> invertedFiles;
-        PorterStemmer stemObject = new PorterStemmer();
-        String[] stopWords;
-
-
-        public PhraseSearching() {
-//            working = files;
-//            stopWords = files.getStopWordsAsArr();
-            System.out.println("The phrase");
-            stopWords[0]="name";
-        }
-
-
-        private String[] SplitQuery(String searchQuery) {
-            String[] subStrings = searchQuery.trim().split("\\s+");
-            return subStrings;
-        }
-
-        private static String[] removeElement(String[] arr, int[] index) {
-            List<String> list = new ArrayList<>(Arrays.asList(arr));
-            for (int i = 0; i < index.length; i++) {
-                list.remove(new String(arr[index[i]]));
-            }
-            return list.toArray(String[]::new);
-        }
-
-
-        private String[] removeStopWords(String[] searchQuery) {
-            int length = searchQuery.length;
-            ArrayList<Integer> indeces = new ArrayList<Integer>();
-            for (int i = 0; i < length; i++) {
-                System.out.println(searchQuery[i].toLowerCase());
-                if (Arrays.asList(this.stopWords).contains(searchQuery[i].toLowerCase())) {
-                    indeces.add(i);
-                }
-            }
-            searchQuery = removeElement(searchQuery, indeces.stream().mapToInt(Integer::intValue).toArray());
-            return searchQuery;
-        }
-
-
-        public JSONArray run(String message, ArrayList<String> queryLinesResult, JSONArray dividedQuery) throws FileNotFoundException, JSONException {
-//            invertedFiles = working.getInvertedFiles();
-            boolean[] indexProcessed;
-            Map<Integer, Integer> allIDs = new HashMap<Integer, Integer>();
-            JSONObject divide = new JSONObject();
-            divide.put("Results", message);
-            dividedQuery.put(divide);
-
-
-            ArrayList<String> allWordsResult = new ArrayList<String>();
-
-
-            String[] result = SplitQuery(message);
-            result = removeStopWords(result);
-            indexProcessed = new boolean[result.length];
-            String json = "{ [";
-            StringBuffer jsonFile = new StringBuffer(json);
-            JSONArray finalJsonFile = new JSONArray();
-            int length = result.length;
-            for (int i = 0; i < length; i++) {
-                // Loop over words
-                ArrayList<String> oneWordResult = new ArrayList<String>();
-
-            String path="D:\\Study\\Second Year\\Second Sem\\APT\\New folder (2)\\New folder (2)\\Sreach-Engine\\InvertedFiles_V3\\";
-            path+=result[i].substring(0,2)+".txt";
-            File pathFile=new File(path);
-                QueryProcessing.searchInInvertedFiles(result[i], pathFile,
-                        oneWordResult, false);
-
-                int length_2 = oneWordResult.size();
-                for (int j = 0; j < length_2; j++) {
-                    queryLinesResult.add(oneWordResult.get(j));
-                    // Loop over versions of Words
-
-
-                    String[] splitLine = oneWordResult.get(j).split("\\[");
-                    int length_3 = splitLine.length;
-                    for (int k = 1; k < length_3; k += 2) {
-
-                        // Loop over links of the same version of each Word
-
-                        int End = splitLine[k].indexOf(']');
-                        String temp = splitLine[k].substring(0, End);
-
-                        String[] finalID = temp.split(",");
-                        int ID = Integer.parseInt(finalID[0]);
-                        if (i == 0 && !indexProcessed[i]) {
-                            allIDs.put(ID, 1);
-                            indexProcessed[0] = true;
-                        } else if (!indexProcessed[i] && allIDs.containsKey(ID)) {
-                            allIDs.put(ID, 1 + allIDs.get(ID));
-                            indexProcessed[i] = true;
-                        }
-                    }
-                }
-
-            }
-
-            for (Iterator<Map.Entry<Integer, Integer>> it = allIDs.entrySet().iterator(); it.hasNext(); ) {
-                Map.Entry<Integer, Integer> entry = it.next();
-                if (entry.getValue() < length) {
-                    it.remove();
-                }
-
-                for (Iterator<Map.Entry<Integer, Integer>> iter = allIDs.entrySet().iterator(); it.hasNext(); ) {
-
-                    Map.Entry<Integer, Integer> IDEntry = iter.next();
-
-                    StringBuffer link = new StringBuffer("");
-                    StringBuffer description = new StringBuffer("");
-                    JSONObject Jo = new JSONObject();
-                    dataBaseObject.getLinkByID(IDEntry.getKey(), link, description);
-                    Jo.put("Link", link);
-                    Jo.put("Description", description);
-                    finalJsonFile.put(Jo);
-                }
-
-
-            }
-            return finalJsonFile;
-        }
-    }
+//    static class PhraseSearching {
+//        DataBase dataBaseObject = new DataBase();
+//        private Map<String, File> invertedFiles;
+//        PorterStemmer stemObject = new PorterStemmer();
+//        String[] stopWords;
+//
+//
+//        public PhraseSearching() {
+////            working = files;
+////            stopWords = files.getStopWordsAsArr();
+//            System.out.println("The phrase");
+//            stopWords[0]="name";
+//        }
+//
+//
+//        private String[] SplitQuery(String searchQuery) {
+//            String[] subStrings = searchQuery.trim().split("\\s+");
+//            return subStrings;
+//        }
+//
+//        private static String[] removeElement(String[] arr, int[] index) {
+//            List<String> list = new ArrayList<>(Arrays.asList(arr));
+//            for (int i = 0; i < index.length; i++) {
+//                list.remove(new String(arr[index[i]]));
+//            }
+//            return list.toArray(String[]::new);
+//        }
+//
+//
+//        private String[] removeStopWords(String[] searchQuery) {
+//            int length = searchQuery.length;
+//            ArrayList<Integer> indeces = new ArrayList<Integer>();
+//            for (int i = 0; i < length; i++) {
+//                System.out.println(searchQuery[i].toLowerCase());
+//                if (Arrays.asList(this.stopWords).contains(searchQuery[i].toLowerCase())) {
+//                    indeces.add(i);
+//                }
+//            }
+//            searchQuery = removeElement(searchQuery, indeces.stream().mapToInt(Integer::intValue).toArray());
+//            return searchQuery;
+//        }
+//
+//
+//        public JSONArray run(String message, ArrayList<String> queryLinesResult, JSONArray dividedQuery) throws FileNotFoundException, JSONException {
+////            invertedFiles = working.getInvertedFiles();
+//            boolean[] indexProcessed;
+//            Map<Integer, Integer> allIDs = new HashMap<Integer, Integer>();
+//            JSONObject divide = new JSONObject();
+//            divide.put("Results", message);
+//            dividedQuery.put(divide);
+//
+//
+//            ArrayList<String> allWordsResult = new ArrayList<String>();
+//
+//
+//            String[] result = SplitQuery(message);
+//            result = removeStopWords(result);
+//            indexProcessed = new boolean[result.length];
+//            String json = "{ [";
+//            StringBuffer jsonFile = new StringBuffer(json);
+//            JSONArray finalJsonFile = new JSONArray();
+//            int length = result.length;
+//            for (int i = 0; i < length; i++) {
+//                // Loop over words
+//                ArrayList<String> oneWordResult = new ArrayList<String>();
+//
+//            String path="D:\\Study\\Second Year\\Second Sem\\APT\\New folder (2)\\New folder (2)\\Sreach-Engine\\InvertedFiles_V3\\";
+//            path+=result[i].substring(0,2)+".txt";
+//            File pathFile=new File(path);
+//                QueryProcessing.searchInInvertedFiles(result[i], pathFile,
+//                        oneWordResult, false);
+//
+//                int length_2 = oneWordResult.size();
+//                for (int j = 0; j < length_2; j++) {
+//                    queryLinesResult.add(oneWordResult.get(j));
+//                    // Loop over versions of Words
+//
+//
+//                    String[] splitLine = oneWordResult.get(j).split("\\[");
+//                    int length_3 = splitLine.length;
+//                    for (int k = 1; k < length_3; k += 2) {
+//
+//                        // Loop over links of the same version of each Word
+//
+//                        int End = splitLine[k].indexOf(']');
+//                        String temp = splitLine[k].substring(0, End);
+//
+//                        String[] finalID = temp.split(",");
+//                        int ID = Integer.parseInt(finalID[0]);
+//                        if (i == 0 && !indexProcessed[i]) {
+//                            allIDs.put(ID, 1);
+//                            indexProcessed[0] = true;
+//                        } else if (!indexProcessed[i] && allIDs.containsKey(ID)) {
+//                            allIDs.put(ID, 1 + allIDs.get(ID));
+//                            indexProcessed[i] = true;
+//                        }
+//                    }
+//                }
+//
+//            }
+//
+//            for (Iterator<Map.Entry<Integer, Integer>> it = allIDs.entrySet().iterator(); it.hasNext(); ) {
+//                Map.Entry<Integer, Integer> entry = it.next();
+//                if (entry.getValue() < length) {
+//                    it.remove();
+//                }
+//
+//                for (Iterator<Map.Entry<Integer, Integer>> iter = allIDs.entrySet().iterator(); it.hasNext(); ) {
+//
+//                    Map.Entry<Integer, Integer> IDEntry = iter.next();
+//
+//                    StringBuffer link = new StringBuffer("");
+//                    StringBuffer description = new StringBuffer("");
+//                    JSONObject Jo = new JSONObject();
+//                    dataBaseObject.getLinkByID(IDEntry.getKey(), link, description);
+//                    Jo.put("Link", link);
+//                    Jo.put("Description", description);
+//                    finalJsonFile.put(Jo);
+//                }
+//
+//
+//            }
+//            return finalJsonFile;
+//        }
+//    }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -236,7 +236,7 @@ public class QuerySearch extends HttpServlet {
 
     static class QueryProcessing{
 
-       static DataBase dataBaseObject = new DataBase();
+       //static DataBase dataBaseObject = new DataBase();
         //WorkingFiles working;
         private Map<String, File> invertedFiles;
         public  PorterStemmer stemObject = new PorterStemmer();
@@ -390,21 +390,21 @@ public class QuerySearch extends HttpServlet {
             return temp;
         }
 
-        public static HashMap<String, Double> replaceIDByLink(HashMap<Integer, Double> hm)
-        {
-            StringBuffer link = new StringBuffer("");
-            DataBase dataBaseObject = QueryProcessing.dataBaseObject;
-            StringBuffer description = new StringBuffer("");
-            HashMap<String, Double> temp = new HashMap<String, Double>();
-            for (Iterator<Map.Entry<Integer, Double>> it = hm.entrySet().iterator(); it.hasNext(); )
-            {
-                Map.Entry<Integer, Double> IDEntry = it.next();
-                dataBaseObject.getLinkByID(IDEntry.getKey(), link, description);
-                temp.put(link.toString(), IDEntry.getValue());
-            }
-
-            return temp;
-        }
+//        public static HashMap<String, Double> replaceIDByLink(HashMap<Integer, Double> hm)
+//        {
+//            StringBuffer link = new StringBuffer("");
+//            DataBase dataBaseObject = QueryProcessing.dataBaseObject;
+//            StringBuffer description = new StringBuffer("");
+//            HashMap<String, Double> temp = new HashMap<String, Double>();
+//            for (Iterator<Map.Entry<Integer, Double>> it = hm.entrySet().iterator(); it.hasNext(); )
+//            {
+//                Map.Entry<Integer, Double> IDEntry = it.next();
+//                dataBaseObject.getLinkByID(IDEntry.getKey(), link, description);
+//                temp.put(link.toString(), IDEntry.getValue());
+//            }
+//
+//            return temp;
+//        }
 
         public String run(String message, ArrayList<String> queryLinesResult, JSONArray dividedQuery)
                 throws FileNotFoundException, JSONException {
